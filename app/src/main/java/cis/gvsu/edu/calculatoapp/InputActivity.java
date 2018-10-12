dea0809af2c3fb5f7ec064db50648cb81de3e653
mrmattkennedy <kennemat@mail.gvsu.edu>
2018-10-12 18:21:05 -0400

    Finished calc function and added comments

--- app/src/main/java/cis/gvsu/edu/calculatoapp/InputActivity.java
+++ app/src/main/java/cis/gvsu/edu/calculatoapp/InputActivity.java
@@ -13,22 +13,49 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import edu.gvsu.cis.convcalc.UnitsConverter;
+
+
+/*
+ * @Authors: Matthew Kennedy, Tyler Bruder
+ * @Version: Fall 2018
+ * Android conversion app
+ *
+ */
+
+/*
+ *  Class that is going to take in and handle any input activity.
+ */
+
 public class InputActivity extends AppCompatActivity {
     public static final int VICE_SELECT = 1;
+
+    /* Default mode */
     private String selectedMode = "length";
+    /* Text box for conversion */
     private EditText input1;
+    /* Text box for conversion2 */
     private EditText input2;
+    /*  Unit view */
     private TextView inputUnit1;
+    /*  Unit view */
     private TextView inputUnit2;
+    /*  Calculate button */
     private Button calcBtn;
+    /* Clear Button */
     private Button clearBtn;
+    /* Toggle modes button */
     private Button modeBtn;
 
+    /*
+    *
+    * Setting up the default 'home screen' for the app when launched
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
-
+        //Adding components to the app
         input1 = findViewById(R.id.textInput1);
         input2 = findViewById(R.id.textInput2);
         inputUnit1 = findViewById(R.id.inputUnit1);
@@ -48,13 +75,19 @@ public class InputActivity extends AppCompatActivity {
         calcBtn.setOnClickListener(v -> calculate());;
     }
 
-
+    /* Clears the text fields when called */
     private void clearFields () {
         input1.setText("");
         input2.setText("");
     }
 
+
+    /*
+    * Changes the units and conversions when the mode is changed
+    *
+    */
     private void swapMode() {
+        //If the mode is length, use length units
         if(selectedMode.equals("length")){
             selectedMode = "volume";
             inputUnit1.setText("Gallons");
@@ -65,23 +98,66 @@ public class InputActivity extends AppCompatActivity {
             inputUnit2.setText("Yards");
         }
         changeHints();
+        clearFields();
     }
 
+
+    /*
+    * Updates the text hints when called
+    *
+    */
     private void changeHints() {
         input1.setHint("Enter " + selectedMode + " in " + inputUnit1.getText());
         input2.setHint("Enter " + selectedMode + " in " + inputUnit2.getText());
     }
 
     private void calculate() {
+        double initVal = 0.0;
+        EditText inputToChange = null;
+        TextView fromView = null;
+        TextView toView = null;
 
+        if ( input1.getText().toString().equals("") && input2.getText().toString().equals(""))
+            return;
+
+//        Log.d("MyApp", input2.getText().toString());
+//        Log.d("MyApp", Boolean.toString(!input1.getText().equals("") && !input2.getText().equals("")));
+        if (input2.getText().toString().equals("") ||
+                (!input1.getText().toString().equals("") && !input2.getText().toString().equals(""))) {
+            initVal = Double.parseDouble(input1.getText().toString());
+            inputToChange = input2;
+            toView = inputUnit2;
+            fromView = inputUnit1;
+        }else if (input1.getText().toString().equals("")) {
+            initVal = Double.parseDouble(input2.getText().toString());
+            inputToChange = input1;
+            fromView = inputUnit2;
+            toView = inputUnit1;
+        }
+
+        if (selectedMode.equals("length")) {
+            inputToChange.setText(Double.toString(edu.gvsu.cis.convcalc.UnitsConverter.convert(
+                    initVal,
+                    UnitsConverter.LengthUnits.valueOf( fromView.getText().toString()),
+                    UnitsConverter.LengthUnits.valueOf(toView.getText().toString()))));
+        } else {
+            inputToChange.setText(Double.toString(edu.gvsu.cis.convcalc.UnitsConverter.convert(
+                    initVal,
+                    UnitsConverter.VolumeUnits.valueOf( fromView.getText().toString()),
+                    UnitsConverter.VolumeUnits.valueOf(toView.getText().toString()))));
+        }
     }
-
+    /*
+     * Creates the main menu and puts the settings menu resource file in.
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.settings, menu);
         return true;
     }
-
+    /*
+     * This will get called when an item is selected from the menu.
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.settings) {
@@ -92,15 +168,17 @@ public class InputActivity extends AppCompatActivity {
         return true;
     }
 
+
+    /*
+     * When the intent is done, this is called when it comes.
+     */
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         clearFields();
-        Log.d("MyApp", Integer.toString(resultCode));
         if (resultCode == Activity.RESULT_OK) {
-            Log.d("MyApp", "HERE");
             inputUnit1.setText(data.getStringExtra("FromUnit"));
             inputUnit2.setText(data.getStringExtra("ToUnit"));
-
+            changeHints();
         }
     }
 }
